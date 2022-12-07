90b28b8cdde875a12f250493503549ecd6caa384
Benson Chen <tumlivein@gmail.com>
2022-12-07 08:26:15 +0000

    test: mock axios implementation

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App, { getFirstAlbumTitle } from './App';
+import { getFirstAlbumTitle } from './App';
+
+// 1. import the module we want to mock
+import axios from 'axios';
+
+// 2. mock the entire module, 
+jest.mock('axios');
 
 describe('getFirstAlbumTitle', () => {
   describe('by default', () => {
     it('should return quidem molestiae enim', async () => {
+      // mock the return value
+      axios.get.mockResolvedValue({
+        data: [
+          {
+            title: 'My 1st title',
+          }
+        ]
+      })
+
       const title = await getFirstAlbumTitle();  // Run the function
-      expect(title).toEqual('quidem molestiae enim');
+      expect(title).toEqual('My 1st title');
     });
   })
 });
